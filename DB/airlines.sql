9c5cd5063e34a9343b436636fe96fb9a16357c51
ashwin <ashwin@usistech.com>
2019-07-14 07:35:31 +0530

    DB for booking

--- DB/airlines.sql
+++ DB/airlines.sql
@@ -1,99 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 4.8.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 13, 2019 at 05:56 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 5.6.36
+-- Adminer 4.7.0 MySQL dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `airlines`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
-  `id` int(5) NOT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `password` varchar(30) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `modified_at` datetime NOT NULL
+  `modified_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
+DROP TABLE IF EXISTS `booking`;
+CREATE TABLE `booking` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `booking_id` int(10) NOT NULL,
+  `from` varchar(30) NOT NULL,
+  `to` varchar(30) NOT NULL,
+  `plane` varchar(30) NOT NULL,
+  `date` datetime NOT NULL,
+  `price` int(10) NOT NULL,
+  `payment_type` varchar(30) NOT NULL,
+  `booking_type` int(2) NOT NULL DEFAULT '1',
+  `total_price` int(10) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `modified_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(5) NOT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `mobile` varchar(30) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`, `status`, `created_at`, `modified_at`) VALUES
-(1, 'ashwinsas', 'ashwin@gmail.com', 'password', '239872348', 1, '2019-07-13 15:27:02', '2019-07-13 15:27:02');
+(4,	'ashwin',	'ashwin',	'ashwin',	'ashwin',	1,	'2019-07-13 15:25:21',	'2019-07-13 15:25:21'),
+(5,	'ashwinsas',	'ashwin@gmail.com',	'password',	'239872348',	1,	'2019-07-13 15:27:02',	'2019-07-13 15:27:02');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2019-07-14 02:04:27
